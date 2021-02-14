1c040da02293df7a42547608092269affcd2a39a
Vijay Kumar Palasala <vijaymoto@gmail.com>
2021-02-14 20:45:15 +1100

    first commit

---
+++ src/main/java/com/example/phonenosdb/PhoneNosDbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.phonenosdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneNosDbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneNosDbServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/phonenosdb/PhoneNosDbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phonenosdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneNosDbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
